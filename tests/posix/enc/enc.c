ab30679110d52b83abfe57fda372e279370a1119
Mike Brasher <mikbras@microsoft.com>
2020-02-12 17:30:47 -0600

    add new posix test

---
+++ tests/posix/enc/enc.c
@@ -0,0 +1,20 @@
+// Copyright (c) Open Enclave SDK contributors.
+// Licensed under the MIT License.
+
+#include <openenclave/corelibc/string.h>
+#include <openenclave/enclave.h>
+#include <openenclave/internal/print.h>
+#include "posix_t.h"
+
+void posix_test(void)
+{
+    oe_host_printf("posix_test()\n");
+}
+
+OE_SET_ENCLAVE_SGX(
+    1,    /* ProductID */
+    1,    /* SecurityVersion */
+    true, /* AllowDebug */
+    1024, /* HeapPageCount */
+    1024, /* StackPageCount */
+    2);   /* TCSCount */
---
+++ tests/posix/host/host.c
@@ -0,0 +1,38 @@
+// Copyright (c) Open Enclave SDK contributors.
+// Licensed under the MIT License.
+
+#include <limits.h>
+#include <openenclave/host.h>
+#include <openenclave/internal/error.h>
+#include <openenclave/internal/tests.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "posix_u.h"
+
+int main(int argc, const char* argv[])
+{
+    oe_result_t result;
+    oe_enclave_t* enclave = NULL;
+    const uint32_t flags = oe_get_create_flags();
+    const oe_enclave_type_t type = OE_ENCLAVE_TYPE_SGX;
+
+    if (argc != 2)
+    {
+        fprintf(stderr, "Usage: %s ENCLAVE_PATH\n", argv[0]);
+        return 1;
+    }
+
+    result = oe_create_posix_enclave(argv[1], type, flags, NULL, 0, &enclave);
+    OE_TEST(result == OE_OK);
+
+    result = posix_test(enclave);
+    OE_TEST(result == OE_OK);
+
+    result = oe_terminate_enclave(enclave);
+    OE_TEST(result == OE_OK);
+
+    printf("=== passed all tests (posix)\n");
+
+    return 0;
+}
