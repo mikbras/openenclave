37640f074ecbb0e4b7ee9ad6a289f97adea33165
Mike Brasher <mikbras@microsoft.com>
2020-02-14 19:04:01 -0600

    more locking fixes

--- 3rdparty/libc/musl/src/thread/vmlock.c
+++ 3rdparty/libc/musl/src/thread/vmlock.c
@@ -5,17 +5,17 @@ static volatile int vmlock[2];
 void __vm_wait()
 {
 	int tmp;
-	while ((tmp=vmlock[0]))
-		__wait(vmlock, vmlock+1, tmp, 1);
+	while ((tmp=__UADDR(vmlock[0])))
+		__wait(&__UADDR(vmlock[0]), &vmlock[1], tmp, 1);
 }
 
 void __vm_lock()
 {
-	a_inc(vmlock);
+	a_inc(&__UADDR(vmlock[0]));
 }
 
 void __vm_unlock()
 {
-	if (a_fetch_add(vmlock, -1)==1 && vmlock[1])
-		__wake(vmlock, -1, 1);
+	if (a_fetch_add(&__UADDR(vmlock[0]), -1)==1 && vmlock[1])
+		__wake(&__UADDR(vmlock[0]), -1, 1);
 }
