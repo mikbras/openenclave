2a9aa92c1cbdf26029234e5f44be6336992202a5
Mike Brasher <mikbras@microsoft.com>
2020-02-22 11:17:37 -0600

    exceptions header

---
+++ 3rdparty/libc/musl/src/posix/posix_exception.h
@@ -0,0 +1,9 @@
+// Copyright (c) Open Enclave SDK contributors.
+// Licensed under the MIT License.
+
+#ifndef _POSIX_EXCEPTION_H
+#define _POSIX_EXCEPTION_H
+
+#define POSIX_EXCEPTION_SIGACTION 0x515d906d
+
+#endif //_POSIX_EXCEPTION_H
