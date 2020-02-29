88608d1ea13d6bf537fb8e896d4895d464a1be83
Mike Brasher <mikbras@microsoft.com>
2020-02-29 13:27:30 -0600

    new tests

---
+++ tests/posix/threads/enc/test_pthread_cancel3.c
@@ -0,0 +1,33 @@
+#include <pthread.h>
+#include <semaphore.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <openenclave/enclave.h>
+#include <openenclave/internal/tests.h>
+
+static void* _thread_func(void *arg)
+{
+    (void)arg;
+
+    for (;;)
+        ;
+
+    return NULL;
+}
+
+int test_pthread_cancel3(void)
+{
+    pthread_t td;
+    void *res;
+
+    printf("=== test_pthread_cancel3()\n");
+    fflush(stdout);
+
+    OE_TEST(pthread_create(&td, 0, _thread_func, NULL) == 0);
+    OE_TEST(pthread_cancel(td) == 0);
+    OE_TEST(pthread_join(td, &res) == 0);
+    OE_TEST(res == PTHREAD_CANCELED);
+
+    return 0;
+}
